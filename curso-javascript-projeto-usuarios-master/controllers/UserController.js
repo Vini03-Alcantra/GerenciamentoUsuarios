448331752273bbda29e41460f05a205c20fc27ca
Vinicius De Alcantra <viniciusalcantarabarros@gmail.com>
2020-12-08 12:57:20 -0300

    Criando método addLine

--- curso-javascript-projeto-usuarios-master/controllers/UserController.js
+++ curso-javascript-projeto-usuarios-master/controllers/UserController.js
@@ -9,7 +9,7 @@ class UserController{
             event.preventDefault();
             this.getValues();
         })
-    }
+    }//fechando onSubmit
 
 
 
@@ -30,5 +30,24 @@ class UserController{
         return new User(user.name, user.gender, user.birth, user.country, user.email, user.password, user.photo, user.admin)
         
         
+    }//fechando getValues
+
+
+    addLine(dataUser, tableId){
+        document.getElementById(tableId).innerHTML = `
+        <tr>
+            <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+            <td>${dataUser.name}</td>
+            <td>${dataUser.email}</td>
+            <td>${dataUser.admin}</td>
+            <td>${dataUser.birth}</td>
+            <td>
+                <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+            </td>
+        </tr>                                    
+        `;
+    
     }
+    
 }
