3e5134f3cfc6627fccc2966f3137e2a9ec5db0c3
Vinicius De Alcantra <viniciusalcantarabarros@gmail.com>
2020-12-08 10:49:13 -0300

    Corrigindo código

--- curso-javascript-projeto-usuarios-master/index.js
+++ curso-javascript-projeto-usuarios-master/index.js
@@ -2,9 +2,7 @@ var fields = document.querySelectorAll("#form-user-create [name]");
 var user = {};
 
 function addLine(dataUser){
-    var tr = document.createElement("tr");
-
-    tr.innerHTML = `
+    document.getElementById("table-users").innerHTML = `
     <tr>
         <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
         <td>${dataUser.name}</td>
